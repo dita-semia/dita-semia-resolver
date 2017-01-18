871ceef3186ebaf94657f2b06e09717e7d642972
Patrik Stellmann <patrik.stellmann@gdv-dl.de>
2017-01-18 10:18:58 +0100

    improved layout

--- java/oxygen-extension-bundle/src/org/DitaSemia/Oxygen/AuthorOperations/RefreshBookCache.java
+++ java/oxygen-extension-bundle/src/org/DitaSemia/Oxygen/AuthorOperations/RefreshBookCache.java
@@ -1,10 +1,14 @@
 package org.DitaSemia.Oxygen.AuthorOperations;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Frame;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
 import org.DitaSemia.Base.ProgressListener;
@@ -37,13 +41,24 @@ public class RefreshBookCache implements AuthorOperation {
 			final Frame parentFrame = (Frame)authorAccess.getWorkspaceAccess().getParentFrame();
 
 		    final JDialog dialog = new JDialog(parentFrame, "Refreshing Book Cache...", true);
+		    JPanel barPanel = new JPanel();
 		    JProgressBar progressBar = new JProgressBar(0, 1);
-		    progressBar.setBorder(BorderFactory.createEmptyBorder(5,  5,  5,  5));
-		    dialog.add(BorderLayout.CENTER, progressBar);
+		    dialog.getContentPane().setLayout(new BoxLayout(dialog.getContentPane(), BoxLayout.PAGE_AXIS));
+		    barPanel.setLayout(new BoxLayout(barPanel, BoxLayout.LINE_AXIS));
+		    
+		    barPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		    barPanel.add(progressBar);
+		    progressBar.setMaximumSize(new Dimension(272, 27));
+		    barPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+		    
+		    dialog.add(Box.createRigidArea(new Dimension(0, 5)));
+		    dialog.add(barPanel);
+		    dialog.add(Box.createRigidArea(new Dimension(0, 5)));
+		    
 		    dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		    dialog.setSize(300, 65);
+		    dialog.setSize(300, 75);
 		    dialog.setLocationRelativeTo(parentFrame);
-
+		    
 		    final ProgressListener progressListener = new ProgressListener() {
 				@Override
 				public void setProgress(int progress, int total) {
